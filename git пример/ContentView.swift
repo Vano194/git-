830695ec4e3735e76af02378fdb3aae64bc15d6c
avan <avan>
2024-07-12 19:43:32 +0300

    change to purple

--- git пример/ContentView.swift
+++ git пример/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             
             NavigationLink(destination: ModelView()) {
                 Text("Go next")
-            } .foregroundColor(.green)
+            } .foregroundColor(.purple)
           
             .navigationBarTitle("xcode and git")
         }
